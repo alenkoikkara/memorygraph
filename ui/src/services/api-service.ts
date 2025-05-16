1df411a65754987164af2d532d012d5188481fec
alenkoikkara <alendennis77@gmail.com>
2025-05-16 14:18:35 -0500

    fix dist

--- ui/src/services/api-service.ts
+++ ui/src/services/api-service.ts
@@ -1,19 +1,14 @@
 import axios from 'axios';
-import { loadEnv } from 'vite';
-
-const env = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 const apiService = axios.create({
-  baseURL: env.VITE_BACKEND_BASE_URL,
+  baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
 });
 
 export default apiService;
 
 
 export const getSearchResults = async (query: string) => {
-  console.log(env.VITE_BACKEND_BASE_URL);
+  console.log(import.meta.env.VITE_BACKEND_BASE_URL);
   const response = await apiService.get(`/search?query=${query}`);
   return response.data;
 };
-
-

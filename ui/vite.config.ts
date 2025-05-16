790c9aebd50baf543ada49af3a1b776a449b18cf
alenkoikkara <alendennis77@gmail.com>
2025-05-16 14:15:05 -0500

    fix vite

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(() => {
-  // Load env file based on `mode` in the current working directory.
-  return {
-    plugins: [react(), tailwindcss()],
-  };
-});
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react(), tailwindcss()],
+})
